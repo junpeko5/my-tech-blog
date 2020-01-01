630f9d5584480b89cbb121bc1a36ca006640623b
junpeko5 <junpeko5@gmail.com>
2020-01-01 15:09:16 +0900

    拡張子をjsxに変更

--- src/components/Avatar/BigAvatar.jsx
+++ src/components/Avatar/BigAvatar.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import siteConfig from "../../../data/SiteConfig";
-
 /** @jsx jsx */
 import { Styled, jsx } from "theme-ui";
 
-const BigAvatar = props => {
+import siteConfig from "../../../data/SiteConfig";
+
+const BigAvatar = () => {
   const { avatar } = siteConfig;
 
   return (
     <>
       <div sx={{ textAlign: `center` }}>
-        <img src={avatar.photo} sx={{ width: 100, borderRadius: 999 }} />
+        <img
+          src={avatar.photo}
+          alt="profileImage"
+          sx={{ width: 100, borderRadius: 999 }}
+        />
         <Styled.h1 sx={{ color: `text` }}>
-          Hello World!!{" "}
+          Hello World!!
         </Styled.h1>
       </div>
       <Styled.p>{avatar.description}</Styled.p>
