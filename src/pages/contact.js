6f2426f8ef320c2fa8ad980ab00df4b6cedeccd1
junpeko5 <junpeko5@gmail.com>
2022-01-17 23:54:28 +0900

    classをFunctionへ変更

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { navigate } from 'gatsby';
 import Recaptcha from 'react-google-recaptcha';
 import Helmet from 'react-helmet';
@@ -14,21 +14,17 @@ function encode(data) {
     .join('&');
 }
 
-export default class Contact extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+const Contact = (props) => {
+  const [state, setState] = useState({});
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  handleRecaptcha = (value) => {
-    this.setState({ 'g-recaptcha-response': value });
+  const handleRecaptcha = (value) => {
+    setState({ ...state, 'g-recaptcha-response': value });
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     fetch('/', {
@@ -36,110 +32,107 @@ export default class Contact extends Component {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
         'form-name': form.getAttribute('name'),
-        ...this.state,
+        ...state,
       }),
     })
       .then(() => navigate(form.getAttribute('action')))
       .catch((error) => alert(error));
   };
 
-  render() {
-    return (
-      <Layout>
-        <Helmet title={`Contact | ${config.siteTitle}`} />
-        <div>
-          <Heading as="h1">お問い合わせ</Heading>
+  return (
+    <Layout>
+      <Helmet title={`Contact | ${config.siteTitle}`} />
+      <div>
+        <Heading as="h1">お問い合わせ</Heading>
+        <Box
+          as="form"
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          data-netlify-recaptcha="true"
+          onSubmit={handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <noscript>
+            <p>This form won’t work with Javascript disabled</p>
+          </noscript>
+          <p hidden>
+            <label>
+              Don’t fill this out:{' '}
+              <input name="bot-field" onChange={handleChange} />
+            </label>
+          </p>
           <Box
-            as="form"
-            name="contact"
-            method="post"
-            action="/thanks/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            data-netlify-recaptcha="true"
-            onSubmit={this.handleSubmit}
+            fontWeight="bold"
+            as="label"
+            htmlFor="name"
+            sx={{
+              color: 'text',
+            }}
           >
-            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-            <input type="hidden" name="form-name" value="contact" />
-            <noscript>
-              <p>This form won’t work with Javascript disabled</p>
-            </noscript>
-            <p hidden>
-              <label>
-                Don’t fill this out:{' '}
-                <input name="bot-field" onChange={this.handleChange} />
-              </label>
-            </p>
-            <Box
-              fontWeight="bold"
-              as="label"
-              htmlFor="name"
-              sx={{
-                color: 'text',
-              }}
-            >
-              名前
-            </Box>
-            <Input
-              sx={{
-                variant: 'forms.input',
-              }}
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              mb="4"
-            />
-            <Box
-              fontWeight="bold"
-              as="label"
-              htmlFor="email"
-              sx={{
-                color: 'text',
-              }}
-            >
-              メールアドレス
-            </Box>
-            <Input
-              sx={{
-                variant: 'forms.input',
-              }}
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              mb="4"
-            />
-            <Box
-              fontWeight="bold"
-              as="label"
-              htmlFor="message"
-              sx={{
-                color: 'text',
-              }}
-            >
-              お問い合わせ内容
-            </Box>
-            <Textarea
-              sx={{
-                variant: 'forms.textarea',
-              }}
-              name="message"
-              value={this.state.message}
-              onChange={this.handleChange}
-              rows="6"
-              mb="4"
-            />
-            <Recaptcha
-              sitekey={RECAPTCHA_KEY}
-              onChange={this.handleRecaptcha}
-            />
-            <Button type="submit" colorScheme="blue" my="4">
-              送信
-            </Button>
+            名前
           </Box>
-        </div>
-      </Layout>
-    );
-  }
-}
+          <Input
+            sx={{
+              variant: 'forms.input',
+            }}
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={handleChange}
+            mb="4"
+          />
+          <Box
+            fontWeight="bold"
+            as="label"
+            htmlFor="email"
+            sx={{
+              color: 'text',
+            }}
+          >
+            メールアドレス
+          </Box>
+          <Input
+            sx={{
+              variant: 'forms.input',
+            }}
+            type="email"
+            name="email"
+            value={state.email}
+            onChange={handleChange}
+            mb="4"
+          />
+          <Box
+            fontWeight="bold"
+            as="label"
+            htmlFor="message"
+            sx={{
+              color: 'text',
+            }}
+          >
+            お問い合わせ内容
+          </Box>
+          <Textarea
+            sx={{
+              variant: 'forms.textarea',
+            }}
+            name="message"
+            value={state.message}
+            onChange={handleChange}
+            rows="6"
+            mb="4"
+          />
+          <Recaptcha sitekey={RECAPTCHA_KEY} onChange={handleRecaptcha} />
+          <Button type="submit" colorScheme="blue" my="4">
+            送信
+          </Button>
+        </Box>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
